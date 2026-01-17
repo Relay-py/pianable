00cb1a1b656246b8308e2f2a4abc622533dfbb1f
Relay-py <rayanelaabid@gmail.com>
2026-01-17 13:20:32 -0500

    get all the bottom and top point to draw the piano for white keys

---
+++ instrument_front.py
@@ -0,0 +1,20 @@
+from instrument.instrument_arm import InstrumentArm
+import numpy as np
+
+
+
+class InstrumentFront():
+    def __init__(self,piano_corners,num_white_keys):
+        self.piano_corners = piano_corners
+        self.num_keys = num_white_keys
+    
+    def get_all_keys():
+        lines_bottom_point = [(self.piano_corners[0]-self.piano_corners[1])*i+self.piano_corners[0] for i in range(num_keys+1)]
+        lines_top_point = [(self.piano_corners[2]-self.piano_corners[3])*i+self.piano_corners[2]   for i in range(num_keys+1)]
+        lines_top_point_draw = [if i%3==0 || i%7==0 then None 
+                                else ((lines_top_point[i]-lines_bottom_point[i])*3/5)+lines_bottom_point[i]
+                                for i in range(lines_top_point)]
+        return (lines_top_point , lines_bottom_point)
+    
+   
+
