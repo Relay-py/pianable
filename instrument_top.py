80eaeffa712538f4be63bbad5b81b48f1ec20a21
Justin <thisisjustinmao@gmail.com>
2026-01-17 13:33:10 -0500

    Fix instrument_top

--- instrument_top.py
+++ instrument_top.py
@@ -1,19 +1,22 @@
-from instrument.instrument_arm import InstrumentArm
 import numpy as np
 
 
-
-class InstrumentFront():
+class InstrumentTop():
     def __init__(self,piano_corners,num_white_keys):
         self.piano_corners = piano_corners
         self.num_keys = num_white_keys
     
-    def get_all_keys():
+    def get_all_keys(self, num_keys):
+        
         lines_bottom_point = [(self.piano_corners[0]-self.piano_corners[1])*i+self.piano_corners[0] for i in range(num_keys+1)]
         lines_top_point = [(self.piano_corners[2]-self.piano_corners[3])*i+self.piano_corners[2]   for i in range(num_keys+1)]
-        lines_top_point_draw = [if i%3==0 || i%7==0 then None 
-                                else ((lines_top_point[i]-lines_bottom_point[i])*3/5)+lines_bottom_point[i]
-                                for i in range(lines_top_point)]
+        lines_top_point_draw = [
+            None if i % 3 == 0 or i % 7 == 0
+            else ((lines_top_point[i] - lines_bottom_point[i]) * 3 / 5) + lines_bottom_point[i]
+            for i in range(len(lines_top_point))
+        ]
+
+        
         return (lines_top_point , lines_bottom_point)
     
    
